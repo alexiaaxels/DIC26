c772d9af638ca49e7d17f4f2c009f5eabcbed41b
Kinga Koltai <koltai@kth.se>
2026-09-12 15:37:55 +0200

    gitignore + data paths

--- Week1/test.py
+++ Week1/test.py
@@ -13,24 +13,28 @@ taxiZones = (
     spark.read
     .option("header", True)
     .option("inferSchema", True)
-    .csv("taxi_zone_lookup.csv")
+    .csv("Data/taxi_zone_lookup.csv")
 )
 
 weather = (
     spark.read
     .option("header", True)
     .option("inferSchema", True)
-    .csv("weather.csv")
+    .csv("Data/weather.csv")
 )
 
 airQuality = (
     spark.read
     .option("header", True)
     .option("inferSchema", True)
-    .csv("hourly_88101_2024.csv")
+    .csv("Data/hourly_88101_2024.csv")
 )
 
-taxiTrips = spark.read.parquet("*.parquet")
+taxiTrips = spark.read.parquet(
+    "Data/yellow_tripdata_2024-01.parquet",
+    "Data/yellow_tripdata_2024-02.parquet",
+    "Data/yellow_tripdata_2024-03.parquet"
+)
 
 
 ### PRINT SCHEMA:
