20094350a368bc4036d0b221e873a6850ccb974d
Gudny Eva Eiriksdottir <gudny@gangverk.is>
2026-09-13 16:11:31 +0200

    clean up

--- Week1/data_config.py
+++ Week1/data_config.py
@@ -31,42 +31,11 @@ def taxi_trips_timestamp(df):
           .withColumn('time_utc', date_trunc('hour', col('pickup_time_utc')).cast('timestamp_ntz'))
     )
 
+# filters out unknown or NaN locations
 def filter_unknown_locations(df):
     return df.filter(~col('pu_location_id').isin(264, 265) & ~col('do_location_id').isin(264, 265))
 
 DATASET_CONFIGS = {
-    'taxi_trips': {
-        'path': ['data/yellow_tripdata_2024-01.parquet', 'data/yellow_tripdata_2024-02.parquet', 'data/yellow_tripdata_2024-03.parquet'],
-        'format': 'parquet',
-        'expected_cols': {
-            'tpep_pickup_datetime', 'tpep_dropoff_datetime', 'passenger_count', 'trip_distance', 'pu_location_id', 'do_location_id', 'fare_amount'
-        },
-        'key_cols': ['vendor_id', 'pickup_time_local', 'do_location_id'],
-        'timestamp_builder': taxi_trips_timestamp,
-        'numeric_checks': {'trip_distance': (0, None), 'fare_amount': (0, None)},
-        'transform': filter_unknown_locations,
-        'schema_version': '1.0'
-    },
-    'weather': {
-        'path': "data/weather.csv",
-        'format': 'csv',
-        'expected_cols': {'year', 'month', 'day', 'hour', 'temp', 'prcp'},
-        'key_cols': ['year', 'month', 'day', 'hour'],
-        'timestamp_builder': weather_timestamp,
-        'numeric_checks': {'temp': (-90, 60)},
-        'transform': None,
-        'schema_version': '1.0'
-    },
-    'air_quality': {
-        'path': "data/hourly_88101_2024.csv",
-        'format': 'csv',
-        'expected_cols': {'state_code', 'county_code', 'date_gmt', 'time_gmt', 'sample_measurement', 'parameter_code'},
-        'key_cols': ['state_code', 'county_code', 'site_num', 'time_utc'],
-        'timestamp_builder': air_quality_timestamp,
-        'numeric_checks': {'sample_measurement': (0, None)},
-        'transform': None,
-        'schema_version': '1.0'
-    },
     'taxi_zone': {
         'path': "data/taxi_zone_lookup.csv",
         'format': 'csv',
@@ -76,5 +45,37 @@ DATASET_CONFIGS = {
         'numeric_checks': {},
         'transform': None,
         'schema_version': '1.0'
+        },
+    'weather': {
+        'path': "data/weather.csv",
+        'format': 'csv',
+        'expected_cols': {'year', 'month', 'day', 'hour', 'temp', 'prcp'},
+        'key_cols': ['year', 'month', 'day', 'hour'],
+        'timestamp_builder': weather_timestamp,
+        'numeric_checks': {'temp': (-90, 60)},
+        'transform': None,
+        'schema_version': '1.0'
+        },
+    'air_quality': {
+        'path': "data/hourly_88101_2024.csv",
+        'format': 'csv',
+        'expected_cols': {'state_code', 'county_code', 'site_num', 'poc', 'date_gmt', 'time_gmt', 'sample_measurement'},
+        'key_cols': ['state_code', 'county_code', 'site_num', 'date_local', 'time_local' 'poc'],
+        'timestamp_builder': air_quality_timestamp,
+        'numeric_checks': {'sample_measurement': (0, None)},
+        'transform': None,
+        'schema_version': '1.0'
+        },
+    'taxi_trips': {
+        'path': ['data/yellow_tripdata_2024-01.parquet', 'data/yellow_tripdata_2024-02.parquet', 'data/yellow_tripdata_2024-03.parquet'],
+        'format': 'parquet',
+        'expected_cols': { 'vendor_id', 'ratecode_id', 'tpep_pickup_datetime', 'tpep_dropoff_datetime', 'passenger_count', 'trip_distance', 'pu_location_id', 'do_location_id', 'fare_amount'
+        },
+        'key_cols': [],
+        'timestamp_builder': taxi_trips_timestamp,
+        'numeric_checks': {'trip_distance': (0,None), 'fare_amount': (0, None)},
+        'transform': filter_unknown_locations,
+        'schema_version': '1.0'
     },
+    
 }
